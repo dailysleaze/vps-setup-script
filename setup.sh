bf372e00cfcc2b0a03e64e3cf0d0886d6b6568f7
d <info@dailysleaze.co.uk>
2014-03-28 17:27:32 +0000

    Update

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ email=""
 pubkey=""
 
 # ssh port
-sshPort="5522"
+sshPort="17942"
 
 # setup
 doSetup=true
@@ -36,19 +36,24 @@ if $doSetup ; then
 
     # nano + other apps for add-apt-repository cmd
     # http://stackoverflow.com/a/16032073
-    sudo apt-get -y install nano python-software-properties software-properties-common
+    sudo apt-get -y install nano
+    #WM python-software-properties software-properties-common
 
     # update time
     rm -f /etc/localtime
-    ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+    ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
     sudo apt-get -y install ntp
     service ntp restart
 
     # bashrc
-    mv ~/.bashrc ~/.bashrc.bak
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/.bashrc
-    mv .bashrc ~/.bashrc
-    chmod 644 ~/.bashrc
+    #mv ~/.bashrc ~/.bashrc.bak
+    #wget https://raw.github.com/amnah/vps-setup-script/master/files/.bashrc
+    #mv .bashrc ~/.bashrc
+    #chmod 644 ~/.bashrc
+
+
+    #WM new user
+    adduser dailysleaze
 
     # prevent root login with password (ssh keys only)
     mkdir ~/.ssh
@@ -57,12 +62,12 @@ if $doSetup ; then
     chmod 700 ~/.ssh 
     chmod 600 ~/.ssh/authorized_keys 
     chown -R root.root .ssh
-    echo -e "\n\nPermitRootLogin no\nPasswordAuthentication no\n#AllowUsers username@(your-ip) username@(another-ip-if-any)" >> /etc/ssh/sshd_config
+    echo -e "\n\nPermitRootLogin no\nPasswordAuthentication no\n#AllowUsers dailysleaze" >> /etc/ssh/sshd_config
     sed -i "s/Port 22/Port $sshPort/g" /etc/ssh/sshd_config
 
-    # fail2ban and logwatch
-    sudo apt-get -y install fail2ban logwatch
-    sed -i "s/--output mail/--output mail --mailto $email --detail high/g" /etc/cron.daily/00logwatch
+    # fail2ban
+    sudo apt-get -y install fail2ban
+    #sed -i "s/--output mail/--output mail --mailto $email --detail high/g" /etc/cron.daily/00logwatch
 
     # restart ssh and fail2ban services
     service ssh restart
@@ -79,7 +84,7 @@ if $doWebServer ; then
     #sudo add-apt-repository -y ppa:git-core/ppa
     sudo apt-get update
     sudo apt-get -y purge apache2* libapache2*
-    sudo apt-get -y install git php5 php5-cli mysql-server mysql-client nginx php5-fpm php5-mysql php5-gd php5-imagick php5-mcrypt php5-memcache php-apc php5-curl curl 
+    sudo apt-get -y install git php5 php5-cli mysql-server mysql-client php5-fpm php5-mysql php5-gd php5-mcrypt php5-memcache php-apc php5-curl curl 
     #sudo apt-get -y install php5-suhosin php5-intl php-pear php5-imap php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
     #nano /etc/php5/cli/conf.d/ming.ini # change "#" to ";"
 
@@ -87,70 +92,69 @@ if $doWebServer ; then
     sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
 
     # set up nginx
-    mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-    mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
-    rm /etc/nginx/sites-enabled/default
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/nginx.conf  -O /etc/nginx/nginx.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/sites-available/_baseApps -O /etc/nginx/sites-available/_baseApps
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/sites-available/_common -O /etc/nginx/sites-available/_common
+    #mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+    #mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+    #rm /etc/nginx/sites-enabled/default
+    #wget https://raw.github.com/amnah/vps-setup-script/master/files/nginx.conf  -O /etc/nginx/nginx.conf
+    #wget https://raw.github.com/amnah/vps-setup-script/master/files/sites-available/_baseApps -O /etc/nginx/sites-available/_baseApps
+    #wget https://raw.github.com/amnah/vps-setup-script/master/files/sites-available/_common -O /etc/nginx/sites-available/_common
 
     # set up data dir
-    mkdir -p /data/sites /data/logs
-    ln -s /etc/nginx/nginx.conf /data/nginx.conf
-    ln -s /etc/nginx/sites-available/ /data
-    ln -s /etc/nginx/sites-enabled/ /data
-    ln -s /etc/nginx/sites-available/_baseApps /etc/nginx/sites-enabled/_baseApps
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/example.site -O /data/example.site
+    #mkdir -p /data/sites /data/logs
+    #ln -s /etc/nginx/nginx.conf /data/nginx.conf
+    #ln -s /etc/nginx/sites-available/ /data
+    #ln -s /etc/nginx/sites-enabled/ /data
+    #ln -s /etc/nginx/sites-available/_baseApps /etc/nginx/sites-enabled/_baseApps
+    #wget https://raw.github.com/amnah/vps-setup-script/master/files/example.site -O /data/example.site
 
     # download and install/move phpMyAdmin
     # note: file gets named "download"
-    wget http://sourceforge.net/projects/phpmyadmin/files/latest/download
-    unzip -q download 
-    rm -f download
-    mv phpMyAdmin* /data
-    ln -s /data/phpMyAdmin* /data/phpMyAdmin
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/config.inc.php -O /data/phpMyAdmin/config.inc.php
+    #wget http://sourceforge.net/projects/phpmyadmin/files/latest/download
+    #unzip -q download 
+    #rm -f download
+    #mv phpMyAdmin* /data
+    #ln -s /data/phpMyAdmin* /data/phpMyAdmin
+    #wget https://raw.github.com/amnah/vps-setup-script/master/files/config.inc.php -O /data/phpMyAdmin/config.inc.php
 
     # setup default + phpMyAdmin logs in nginx
-    mkdir /data/logs/_
-    touch /data/logs/_/access.log
-    touch /data/logs/_/error.log
-    mkdir /data/logs/phpMyAdmin
-    touch /data/logs/phpMyAdmin/access.log
-    touch /data/logs/phpMyAdmin/error.log
+    #mkdir /data/logs/_
+    #touch /data/logs/_/access.log
+    #touch /data/logs/_/error.log
+    #mkdir /data/logs/phpMyAdmin
+    #touch /data/logs/phpMyAdmin/access.log
+    #touch /data/logs/phpMyAdmin/error.log
 
     # add logrotate to site logs and change rotation settings
-    sed -i "s/*.log/*.log \/data\/logs\/*\/*.log/g" /etc/logrotate.d/nginx
-    sed -i "s/daily/size=50M/g" /etc/logrotate.d/nginx
-    sed -i "s/daily/size=50M/g" /etc/logrotate.d/mysql-server
-    sed -i "s/rotate 7/rotate 52/g" /etc/logrotate.d/mysql-server
+    #sed -i "s/*.log/*.log \/data\/logs\/*\/*.log/g" /etc/logrotate.d/nginx
+    #sed -i "s/daily/size=50M/g" /etc/logrotate.d/nginx
+    #sed -i "s/daily/size=50M/g" /etc/logrotate.d/mysql-server
+    #sed -i "s/rotate 7/rotate 52/g" /etc/logrotate.d/mysql-server
 
     # add fail2ban configurations
     # http://snippets.aktagon.com/snippets/554-how-to-secure-an-nginx-server-with-fail2ban
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/proxy.conf -O /etc/fail2ban/filter.d/proxy.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-auth.conf -O /etc/fail2ban/filter.d/nginx-auth.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-login.conf -O /etc/fail2ban/filter.d/nginx-login.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-noscript.conf -O /etc/fail2ban/filter.d/nginx-noscript.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-dos.conf -O /etc/fail2ban/filter.d/nginx-dos.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/jail.local.tmp -O /etc/fail2ban/jail.local.tmp
+    # wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/proxy.conf -O /etc/fail2ban/filter.d/proxy.conf
+    # wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-auth.conf -O /etc/fail2ban/filter.d/nginx-auth.conf
+    # wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-login.conf -O /etc/fail2ban/filter.d/nginx-login.conf
+    # wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-noscript.conf -O /etc/fail2ban/filter.d/nginx-noscript.conf
+    # wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-dos.conf -O /etc/fail2ban/filter.d/nginx-dos.conf
+    # wget https://raw.github.com/amnah/vps-setup-script/master/files/jail.local.tmp -O /etc/fail2ban/jail.local.tmp
 
     # combine the tmp jail.local.tmp into the preconfigured jail.conf
-    cat /etc/fail2ban/jail.conf /etc/fail2ban/jail.local.tmp > /etc/fail2ban/jail.local
-    rm /etc/fail2ban/jail.local.tmp
+    #cat /etc/fail2ban/jail.conf /etc/fail2ban/jail.local.tmp > /etc/fail2ban/jail.local
+    #rm /etc/fail2ban/jail.local.tmp
 
     # restart nginx services
     service php5-fpm restart
-    service nginx restart
+    service apache2 restart
 
     # change owner and permissions
-    chown -R www-data.www-data /data/sites
-    find /data/sites -type d -print0 | xargs -0 chmod 0755
-    #find /data/sites -type f -print0 | xargs -0 chmod 0644 # NOPE dont need this
+    #chown -R www-data.www-data /data/sites
+    #find /data/sites -type d -print0 | xargs -0 chmod 0755    
 
     # clean up and download site.sh and backup.sh
-    wget https://raw.github.com/amnah/vps-setup-script/master/site.sh
-    wget https://raw.github.com/amnah/vps-setup-script/master/backup.sh
-    chmod 700 site.sh backup.sh
+    #wget https://raw.github.com/amnah/vps-setup-script/master/site.sh
+    #wget https://raw.github.com/amnah/vps-setup-script/master/backup.sh
+    #chmod 700 site.sh backup.sh
 
     # display message about site.sh
     echo -e "------------------------------------------"
